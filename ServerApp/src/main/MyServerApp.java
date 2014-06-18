511c4414f499ee93263b725f9a343e23ddd1dd16
KrzysiekOwczarek <krzysiek.owczarek@onet.eu>
2014-06-18 22:19:22 +0200

    save

--- ServerApp/src/main/MyServerApp.java
+++ ServerApp/src/main/MyServerApp.java
@@ -10,6 +10,10 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.Date;
 
@@ -83,8 +87,9 @@ public class MyServerApp implements Runnable {
 }
 
 class ClientThread extends Thread {
-	private String clientName = null;
+	private String clientPhoneNum = null;
 	private Socket clientSocket = null;
+	private String clientEventId = null;
 	private DataInputStream is = null;
 	private PrintStream os = null;
 	private BufferedReader reader = null;
@@ -92,8 +97,8 @@ class ClientThread extends Thread {
 	
 	private ArrayList<ClientThread> clientThreads;
 	
-	private int timeout = 10000;
-	private int maxTimeout = 25000;
+	private int timeout = 120000;
+	private int maxTimeout = 180000;
 	
 	volatile long lastReadTime;
 	
@@ -108,17 +113,57 @@ class ClientThread extends Thread {
 	
 	public void run() {
 		try {
-			this.clientSocket.setSoTimeout(timeout);
+			//this.clientSocket.setSoTimeout(timeout);
 			is = new DataInputStream(clientSocket.getInputStream());
 			os = new PrintStream(clientSocket.getOutputStream());
 			reader = new BufferedReader(new InputStreamReader(is));
 			
-			Date date = new Date();
-			this.clientName = "Client_from_" + new Timestamp(date.getTime());
+			//Date date = new Date();
+			//this.clientName = "Client_from_" + new Timestamp(date.getTime());
 			
 			while(isRunning) {
 				try {
+					String[] parts = null;
 					String line = reader.readLine();
+					
+					if(line != null)
+						parts = line.split("\\|");
+					
+					if(parts != null)
+						switch(parts[0]) {
+							case "HELLO": //PHONE_NUM
+								try{
+									this.clientPhoneNum = parts[1];
+									this.sendMsg("self", "HELLO");
+								}catch(ArrayIndexOutOfBoundsException ex) {
+									this.sendMsg("self", "RE|HELLO");
+								}
+								
+								break;
+								
+							case "LOC": //PHONE_NUM|LAT|LON
+								//PRZYJECIE LOKALIZACJI OD USERA
+								this.writeLocToSQL(parts[2], parts[3]);
+								break;
+								
+							case "EVENT": //PHONE_NUM|EVENT_NAME|LAT|LON|DATE
+								//REJESTRACJA EVENTU -> ODESŁANIE PRZYDZIELONEGO UNIQUE ID
+								this.writeEventToSQL(parts[2], parts[3], parts[4], parts[5]);
+								break;
+								
+							case "REG": //PHONE_NUMBER|EVENTID
+								//ZAPISANIE USERA NA WYDARZENIE
+								this.clientEventId = parts[2];
+								//READ FROM EVENT DB
+								break;
+							
+						}
+					
+				}catch(IOException ex) {
+					ex.printStackTrace();
+				}
+					/*
+						
 					
 					lastReadTime = System.currentTimeMillis();
 			        if (line != null && line.startsWith("/quit")) {
@@ -153,6 +198,8 @@ class ClientThread extends Thread {
 				}catch (IOException e) {
 					e.printStackTrace();
 				}
+				
+				*/
 			}
 			
 			this.clientSocket.close();
@@ -162,7 +209,49 @@ class ClientThread extends Thread {
 			ex.printStackTrace();
 		}
 		
-		this.server.log("Client " + this.clientName + " terminated");
+		this.server.log("Client " + this.clientPhoneNum + " terminated");
+	}
+	
+	public void writeLocToSQL(String lat, String lon) {
+		System.out.println("LOC TO SQL: " + this.clientPhoneNum + " on event: " + this.clientEventId + " location: " + lat + " " + lon);
+	}
+	
+	public int writeEventToSQL(String name, String lat, String lon, String date) {
+		System.out.println("EVENT TO SQL: event" + name + " on : " + date + " location: " + lat + " " + lon);
+		
+		return this.getEventIdFromSQL(name, lat, lon, date);
+	}
+	
+	private int getEventIdFromSQL(String name, String lat, String lon, String date) {
+	
+		return 0;
+	}
+	
+	public void setClientEventId(String id) {
+		this.clientEventId = id;
+	}
+	
+	public void sendMsg(String phoneNum, String msg) {
+		if(msg != null)
+			if(phoneNum == "self")
+				this.os.println(msg);
+			else
+	        	for (int i = 0; i < clientThreads.size(); i++) {
+	        		if (clientThreads.get(i) != null && clientThreads.get(i).clientPhoneNum == phoneNum) {
+	        			clientThreads.get(i).os.println(msg);
+	        			this.server.log("Msg: " + msg + " send to client " + clientThreads.get(i).clientPhoneNum);
+	        		}
+	            }
+	}
+	
+	public synchronized void broadcastMsg(String msg) {
+		if(msg != null)
+        	for (int i = 0; i < clientThreads.size(); i++) {
+        		if (clientThreads.get(i) != null && clientThreads.get(i).clientPhoneNum != null) {
+        			clientThreads.get(i).os.println(msg);
+        			this.server.log("Msg: " + msg + " send to client " + clientThreads.get(i).clientPhoneNum);
+        		}
+            }
 	}
 	
 	public synchronized void terminateThread() {
@@ -181,4 +270,54 @@ class ClientThread extends Thread {
 	public void sendHeartBeat() {
 		this.os.println("CHECK_ALIVE");
 	}
+}
+
+class ClientDAO {
+	private final static String DBURL = "jdbc:mysql://127.0.0.1:3306/blog";
+    private final static String DBUSER = "root";
+    private final static String DBPASS = "****";
+    private final static String DBDRIVER = "com.mysql.jdbc.Driver";
+
+    //obiekt tworzący połączenie z bazą danych.
+    private Connection connection;
+    //obiekt pozwalający tworzyć nowe wyrażenia SQL
+    private Statement statement;
+    //zapytanie SQL
+    private String query;
+ 
+    public ClientDAO() { }
+
+    public void writeLocation(String phoneNum, String lat, String lon) {
+    	query = "";
+    	
+    	try {
+            Class.forName(DBDRIVER).newInstance();
+            connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+ 
+            //zwolnienie zasobów i zamknięcie połączenia
+            statement.close();
+            connection.close();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void writeEvent(String phoneNum, String eventName, String lat, String lon, String date) {
+    	query = "";
+    	
+    	try {
+            Class.forName(DBDRIVER).newInstance();
+            connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+ 
+            //zwolnienie zasobów i zamknięcie połączenia
+            statement.close();
+            connection.close();
+        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
